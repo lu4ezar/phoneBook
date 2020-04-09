871be5925a36f5dacedffc245578bb6c8897c618
lu4ezar <misha-shisha@mail.ru>
2020-04-09 18:26:08 +0700

    + App content

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState, SyntheticEvent } from "react";
+import Input from "../input";
+import List from "../list";
+import { PhoneBook } from "../../interfaces";
 
-const App = () => <div>Phone Book</div>
+const App = () => {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState<PhoneBook>([]);
+
+  const handleChange = (e: SyntheticEvent) => {
+    setValue((e.target as HTMLInputElement).value);
+  };
+
+  return (
+    <>
+      <header>Phone Book</header>
+      <main>
+        <Input value={value} onChange={handleChange} />
+        <List list={list} />
+      </main>
+    </>
+  );
+};
 
 export default App;
