7b2cda32d1291e50dec767c3f5f6c14ca1e91c12
lu4ezar <misha-shisha@mail.ru>
2020-04-13 00:05:14 +0700

    excluded 'id' field from search results

--- src/features/phoneBook/selectors.ts
+++ src/features/phoneBook/selectors.ts
@@ -15,9 +15,16 @@ export const selectEditingId = (state: RootState) => state.editingId;
 export const selectFiltered = createSelector(
   [selectPhoneNumbers, selectSearchString],
   (phoneBook, search) => {
-    const filterPhoneNumbers = (phoneNumber: PhoneNumber) =>
-      Object.values(phoneNumber).filter((value) => value.includes(search))
-        .length > 0;
+    const filterPhoneNumbers = (phoneNumber: PhoneNumber) => {
+      const phoneNumberKeys = Object.keys(phoneNumber);
+      const filteredPhoneNumberKeys = phoneNumberKeys.filter((key) => {
+        if (key === "id") {
+          return false;
+        }
+        return phoneNumber[key as keyof PhoneNumber]?.includes(search);
+      });
+      return filteredPhoneNumberKeys.length > 0;
+    };
     return phoneBook.filter(filterPhoneNumbers);
   }
 );
