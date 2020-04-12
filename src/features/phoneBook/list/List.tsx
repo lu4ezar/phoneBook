69010f535cd483cc6e0afa2c3454d4349d17514f
lu4ezar <misha-shisha@mail.ru>
2020-04-12 13:21:15 +0700

    refactor: List

--- src/features/phoneBook/list/List.tsx
+++ src/features/phoneBook/list/List.tsx
@@ -1,23 +1,72 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Phone from "../phone";
-import { fetchPhoneBook, selectPhoneNumbers } from "../phoneBookSlice";
+import Form from "../form";
+import {
+  fetchPhoneBook,
+  selectPhoneBook,
+  deletePhoneNumber,
+  updatePhoneNumber,
+} from "../phoneBookSlice";
+import {
+  editingSelector,
+  setEditingId,
+  dropEditingId,
+} from "../editing/editingSlice";
+import { StyledUl } from "./styled";
+import { PhoneNumber, PhoneNumberId } from "../../../interfaces";
 
 const List = () => {
-  const phoneBook = useSelector(selectPhoneNumbers);
+  const { data, loading, error } = useSelector(selectPhoneBook);
+  const editingId = useSelector(editingSelector);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchPhoneBook());
     return () => {
       dispatch(fetchPhoneBook());
     };
   }, [dispatch]);
+
+  const handleSubmit = (data: PhoneNumber) => {
+    dispatch(updatePhoneNumber(data));
+    dispatch(dropEditingId());
+  };
+
+  const handleDelete = (id: PhoneNumberId) => {
+    dispatch(deletePhoneNumber(id));
+  };
+
+  const handleEdit = (id: PhoneNumberId) => {
+    dispatch(setEditingId(id));
+  };
+
+  const handleCancel = () => {
+    dispatch(dropEditingId());
+  };
+
   return (
-    <ul>
-      {Object.keys(phoneBook).map((id) =>
-        phoneBook[id] ? <Phone key={id} {...phoneBook[id]} /> : null
+    <StyledUl>
+      {Object.keys(data).map((id) =>
+        id === editingId ? (
+          <Form
+            key={id}
+            data={data[id] as PhoneNumber}
+            onCancel={handleCancel}
+            onSubmit={handleSubmit}
+          />
+        ) : data[id] ? (
+          <Phone
+            key={id}
+            data={data[id] as PhoneNumber}
+            onDelete={(e) => handleDelete(id)}
+            onEdit={(e) => handleEdit(id)}
+          />
+        ) : null
       )}
-    </ul>
+      {loading === "pending" && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+    </StyledUl>
   );
 };
 
