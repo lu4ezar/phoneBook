8fa5a9c0f70a757293e74ba402e753ccefbcb38f
lu4ezar <misha-shisha@mail.ru>
2020-04-12 09:07:50 +0700

    moved crud to api folder

--- src/api/crud.ts
+++ src/api/crud.ts
@@ -1,8 +1,8 @@
-import { PhoneNumber } from "./interfaces";
+import { PhoneNumber, PhoneNumberId } from "../interfaces";
 
 const url = "https://phone-book-decentury.herokuapp.com/api/pb/";
 
-export const fetchPhoneBook = () => fetch(url).then((res) => res.json());
+export const fetchPhoneBook = () => fetch(url);
 
 export const createPhoneNumber = (data: PhoneNumber) =>
   fetch(url, {
@@ -11,21 +11,20 @@ export const createPhoneNumber = (data: PhoneNumber) =>
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ data: { ...data } }),
-  }).then((res) => res.json());
+  });
 
-export const readPhoneNumberById = (id: PhoneNumber["id"]) =>
-  fetch(url + id).then((res) => res.json());
+export const readPhoneNumberById = (id: PhoneNumberId) => fetch(url + id);
 
 export const updatePhoneNumber = (data: PhoneNumber) =>
-  fetch(url + data.id, {
+  fetch(url, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ data: { ...data } }),
-  }).then((res) => res.json());
+  });
 
-export const deletePhoneNumber = (id: PhoneNumber["id"]) =>
+export const deletePhoneNumber = (id: PhoneNumberId) =>
   fetch(url + id, {
     method: "DELETE",
-  }).then(() => id);
+  });
