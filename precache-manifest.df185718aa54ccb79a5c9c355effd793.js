ce80bea050f26d23801ba6a5a55082aec1407a17
lu4ezar <misha-shisha@mail.ru>
2020-04-13 00:28:33 +0700

    Updates

---
+++ precache-manifest.df185718aa54ccb79a5c9c355effd793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306059df0f0c3ce08bf39ef082d500fa",
+    "url": "/phoneBook/index.html"
+  },
+  {
+    "revision": "f1bca908d4d3adacf873",
+    "url": "/phoneBook/static/css/main.c95c7188.chunk.css"
+  },
+  {
+    "revision": "8a48702bb9651879afc6",
+    "url": "/phoneBook/static/js/2.287b8cb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phoneBook/static/js/2.287b8cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1bca908d4d3adacf873",
+    "url": "/phoneBook/static/js/main.42b836fc.chunk.js"
+  },
+  {
+    "revision": "b84dc3f7d85d6f2e99bd",
+    "url": "/phoneBook/static/js/runtime-main.f4fc4cb0.js"
+  }
+]);
