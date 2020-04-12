558cfa91274be595c245ea9be96067a656a713a8
lu4ezar <misha-shisha@mail.ru>
2020-04-12 13:19:29 +0700

    updated styles for Phone

--- src/features/phoneBook/phone/styled.ts
+++ src/features/phoneBook/phone/styled.ts
@@ -4,20 +4,10 @@ export const StyledLi = styled.li`
   display: flex;
   flex-wrap: nowrap;
   justify-content: space-between;
-  max-width: 100%;
-  flex: 1;
   border-bottom: 1px solid silver;
   align-items: center;
+  height: 100%;
   & span {
-    flex: 1 0 33%;
+    flex: 1 0 25%;
   }
-`;
-
-export const Text = styled.div`
-  display: flex;
-  flex-grow: 1;
-`;
-
-export const Buttons = styled.div`
-  display: flex;
 `;
