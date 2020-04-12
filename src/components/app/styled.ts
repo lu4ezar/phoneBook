14cb6b74a3bfd4a567e8d5e4722229add99672c0
lu4ezar <misha-shisha@mail.ru>
2020-04-12 13:14:04 +0700

    header now fits common layout

--- src/components/app/styled.ts
+++ src/components/app/styled.ts
@@ -2,14 +2,19 @@ import styled from "styled-components";
 
 export const StyledApp = styled.div`
   height: 100%;
+  width: 60%;
   display: flex;
   flex-direction: column;
-  text-align: center;
-  padding: 0 20%;
+  margin: 0 auto;
   & header {
     background: blanchedalmond;
     width: 100vw;
     align-self: center;
+  }
+  & h1 {
+    width: max-content;
+    position: relative;
+    left: 20%;
   }
   & main {
     padding: 2em 0;
