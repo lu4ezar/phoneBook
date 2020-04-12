063afbb23125c774f6a61c72f82c77bad52391de
lu4ezar <misha-shisha@mail.ru>
2020-04-12 13:22:20 +0700

    updated List styles

---
+++ src/features/phoneBook/list/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledUl = styled.ul`
+  list-style-type: none;
+  margin: 1em 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 100%;
+`;
