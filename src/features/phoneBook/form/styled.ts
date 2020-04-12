b6f5a00b263d11d0758548a4ef3d32c58dd5caee
lu4ezar <misha-shisha@mail.ru>
2020-04-12 13:22:34 +0700

    updated Form styles

---
+++ src/features/phoneBook/form/styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0.5em 0;
+  padding: 0.5em;
+  border-bottom: 1px solid silver;
+  input:not([type="submit"], [type="button"]) {
+    border: none;
+  }
+`;
